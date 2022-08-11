6b798ed68a3a69b04bfd1030cdca2a5cfbe297d2
Mariusz-Lazarz <ilUU0456@gmail.com>
2022-08-11 23:38:30 +0200

    Add classes

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>JustJoinIt</title>
+
+    <link rel="stylesheet" href="css/main.css" /> 
+
+    <script
+      src="https://kit.fontawesome.com/b160ac93e6.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+
+  <body>
+    <div class="offer-container">
+      <div id="offer-box">
+        <div class="offer-img">
+          <img src="img/map.jpg" alt="">
+        </div>
+        <div class="offer-top">
+          <div class="offer-top-box">
+            <p class="offer-name">name</p>
+            <p class="offer-salary">19999</p>
+            <p class="offer-new">new</p>
+          </div>
+        </div>
+      </div>
+    </div>  
+  </body>
+</html>
