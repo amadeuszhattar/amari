85b975e88501602f1da888d98c879418d3434af3
Mariusz-Lazarz <ilUU0456@gmail.com>
2022-08-23 19:58:05 +0200

    updating layout

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>JustJoinIt</title>
 
-    <link rel="stylesheet" href="css/main.css" /> 
+    <link rel="stylesheet" href="css/main.css" />
 
     <script
       src="https://kit.fontawesome.com/b160ac93e6.js"
@@ -15,19 +15,33 @@
   </head>
 
   <body>
-    <div class="offer-container">
-      <div id="offer-box">
-        <div class="offer-img">
-          <img src="img/map.jpg" alt="">
+    <div id="offer-container">
+      <div class="offer-img-box">
+       <img class="offer-img" src="img/map.jpg" alt="">
+      </div>
+      <div class="offer-box">
+        <div class="offer-box-top">
+          <div class="offer-title-box">
+            <h2 class="offer-title">Job title</h2>
+          </div>
+          <div class="offer-salary">
+            <p class="offer-amount">20000 - 99999 PLN</p>
+            <p class="offer-new">New</p>
+          </div>
         </div>
-        <div class="offer-top">
-          <div class="offer-top-box">
-            <p class="offer-name">name</p>
-            <p class="offer-salary">19999</p>
-            <p class="offer-new">new</p>
+        <div class="offer-box-bottom">
+          <div class="offer-location-box">
+            <p class="offer-company">Comapny name</p>
+            <p class="offer-location">Offer location</p>
+            <p class="offer-remote">Remote Job</p>
+          </div>
+          <div class="offer-tech-box">
+            <p class="offer-tech">JavaScript</p>
+            <p class="offer-tech">Html</p>
+            <p class="offer-tech">Css</p>
           </div>
         </div>
       </div>
-    </div>  
+    </div>
   </body>
 </html>
