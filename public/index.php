a6b6f0a74caa15a9c52624e9d00cee805ab3338c
Mariusz-Lazarz <ilUU0456@gmail.com>
2022-08-23 23:38:41 +0200

    Adding map and styles

--- public/index.php
+++ public/index.php
@@ -15,57 +15,44 @@
   </head>
 
   <body>
-    <div id="offer-container">
-      <div class="offer-img-box">
-       <img class="offer-img" src="img/map.jpg" alt="">
-      </div>
-      <div class="offer-box">
-        <div class="offer-box-top">
-          <div class="offer-title-box">
-            <h2 class="offer-title">Job title</h2>
+    <div id="main-container">
+      <div id="content-container">
+        <div id="offer-container">
+          <div class="offer-img-box">
+          <img class="offer-img" src="img/map.jpg" alt="">
           </div>
-          <div class="offer-salary">
-            <p class="offer-amount">20000 - 99999 PLN</p>
-            <p class="offer-new">New</p>
+          <div class="offer-box">
+            <div class="offer-box-top">
+              <div class="offer-title-box">
+                <h2 class="offer-title">Job title</h2>
+              </div>
+              <div class="offer-salary">
+                <p class="offer-amount">20000 - 99999 PLN</p>
+                <p class="offer-new">New</p>
+              </div>
+            </div>
+            <div class="offer-box-bottom">
+              <div class="offer-location-box">
+                <p class="offer-company">Comapny name</p>
+                <p class="offer-location">Offer location</p>
+                <p class="offer-remote">Remote Job</p>
+              </div>
+              <div class="offer-tech-box">
+                <p class="offer-tech">JavaScript</p>
+                <p class="offer-tech">Html</p>
+                <p class="offer-tech">Css</p>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="offer-box-bottom">
-          <div class="offer-location-box">
-            <p class="offer-company">Comapny name</p>
-            <p class="offer-location">Offer location</p>
-            <p class="offer-remote">Remote Job</p>
-          </div>
-          <div class="offer-tech-box">
-            <p class="offer-tech">JavaScript</p>
-            <p class="offer-tech">Html</p>
-            <p class="offer-tech">Css</p>
-          </div>
-        </div>
       </div>
-<<<<<<< HEAD:index.php
+      <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
     </div>
-=======
-    </div>  
-    <script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyA5wWUBjjcJiOYCQ8aDjNUefkYseeP_yzY",
-    authDomain: "amari-e27bd.firebaseapp.com",
-    projectId: "amari-e27bd",
-    storageBucket: "amari-e27bd.appspot.com",
-    messagingSenderId: "545587132611",
-    appId: "1:545587132611:web:5d3d0c836a038a83baca76"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
->>>>>>> fa0fef15c12b7ef210cd48ee6971a515b50fc8bd:public/index.php
+    
+    
   </body>
 
 </html>
+
+
+
