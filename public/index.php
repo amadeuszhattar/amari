8095f3af4814bebcbbbca43e868c380f6b050c1f
Mariusz-Lazarz <ilUU0456@gmail.com>
2022-08-24 18:22:45 +0200

    styling layout

--- public/index.php
+++ public/index.php
@@ -24,29 +24,32 @@
           <div class="offer-box">
             <div class="offer-box-top">
               <div class="offer-title-box">
-                <h2 class="offer-title">Job title</h2>
+                <h2 class="offer-title">Junior front-end developer</h2>
               </div>
               <div class="offer-salary">
-                <p class="offer-amount">20000 - 99999 PLN</p>
+                <p class="offer-amount">20k - 25k</p>
                 <p class="offer-new">New</p>
               </div>
             </div>
             <div class="offer-box-bottom">
               <div class="offer-location-box">
-                <p class="offer-company">Comapny name</p>
-                <p class="offer-location">Offer location</p>
+                <p class="offer-company">Amari benefits it</p>
+                <p class="offer-location">Ireland, Dublin</p>
                 <p class="offer-remote">Remote Job</p>
               </div>
               <div class="offer-tech-box">
                 <p class="offer-tech">JavaScript</p>
-                <p class="offer-tech">Html</p>
-                <p class="offer-tech">Css</p>
+                <p class="offer-tech">JavaScript</p>
+                <p class="offer-tech">JavaScript</p>
+                <p class="offer-tech">html</p>
               </div>
             </div>
           </div>
         </div>
+        
       </div>
       <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="yes" marginheight="0" marginwidth="0"></iframe>
+      <a href="#" class="map-button">Map</a>
     </div>
     
     
