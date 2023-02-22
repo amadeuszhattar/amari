1e7fca3c3d2121d17ffce01e52034228a722bbd5
Mariusz-Lazarz <mariusz.lazarz97@gmail.com>
2023-02-22 11:35:28 +0100

    button fixes

--- layout.js
+++ layout.js
@@ -3,7 +3,7 @@ const offersLeftBtn = document.querySelectorAll('.offers__left--button');
 
 // toggling left nav button
 navLeft.addEventListener('click', function(e){
-    const cur = e.target;
+    const cur = e.target.closest('.offers__left--button');
    if(!cur.classList.contains('offers__left--button--active')){
     offersLeftBtn.forEach(btn => btn.classList.remove('offers__left--button--active'))
     cur.classList.add('offers__left--button--active');
