f425d59b15d12048f4667632bbf978c7fac8a715
Mariusz-Lazarz <mariusz.lazarz97@gmail.com>
2023-03-06 13:49:04 +0100

    adding overlay

--- offers.js
+++ offers.js
@@ -1,12 +1,12 @@
-'use strict'
-// location menu expand 
+"use strict";
+// location menu expand
 // offersBox.addEventListener('click', function(e){
-//     // get elements 
+//     // get elements
 //     const curEl = e.target.closest('.offers__box')
 //     const clicked = e.target.closest('.offers__info--city')
 //     const curElArrow = curEl.closest('div').querySelector('.offers__info--expand--box')
 //     const curElMenu = curEl.closest('div').querySelector('.offers__city--list')
-//     // check if curEl exists 
+//     // check if curEl exists
 //     if(!curEl || !clicked) return
 //     // open menu once clicking on the button
 //     if(clicked.classList.contains('offers__info--city') && !curElArrow.classList.contains('offer__info--expand--active')){
@@ -21,14 +21,13 @@
 //     const clicked = e.target.closest('.offers__city--item')
 //     const curElMenu  = curEl.closest('div').querySelector('.offers__city--list')
 //     const curElArrow = curEl.closest('div').querySelector('.offers__info--expand--box')
-//     if(!clicked) return 
+//     if(!clicked) return
 //     if(clicked){
 //         menuRemove(curElMenu, curElArrow)
-//     } 
+//     }
 // })
 
-
-// // helpers 
+// // helpers
 // const menuRemove = function(curElMenu, curElArrow){
 //     curElArrow.classList.remove('offers__info--expand--active')
 //     curElMenu.classList.remove('offers__city--list--active');
@@ -38,33 +37,76 @@
 //     curElArrow.classList.add('offers__info--expand--active')
 //     curElMenu.classList.add('offers__city--list--active');
 // }
-document.addEventListener('click', function(e){
-    e.preventDefault()
-    const allArrows = document.querySelectorAll('.offers__info--expand--box')
-    const allCityList = document.querySelectorAll('.offers__city--list')
-    const clicked = e.target
-    const expandMenu = clicked.closest('.offers__info--city')
-    if(!expandMenu) {
-        allArrows.forEach(el => el.classList.remove('offers__info--expand--active'))
-        allCityList.forEach(el => el.classList.remove('offers__city--list--active'))
-    } 
-    if(!expandMenu) return
-    const arrow = expandMenu.closest('div').querySelector('.offers__info--expand--box')
-    const offerBox = e.target.closest('.offers__box')
-    const cityList = offerBox.closest('div').querySelector('.offers__city--list')
-    if(!cityList.classList.contains('offers__city--list--active')){
-        allCityList.forEach(el => el.classList.remove('offers__city--list--active'))
-        cityList.classList.add('offers__city--list--active')
-    }
-    if(!arrow.classList.contains('offers__info--expand--active')){
-        allArrows.forEach(el => el.classList.remove('offers__info--expand--active'))
-        arrow.classList.add('offers__info--expand--active')
+// document.addEventListener('click', function(e){
+//     e.preventDefault()
+//     const allArrows = document.querySelectorAll('.offers__info--expand--box')
+//     const allCityList = document.querySelectorAll('.offers__city--list')
+//     const clicked = e.target
+//     const expandMenu = clicked.closest('.offers__info--city')
+//     if(!expandMenu) {
+//         allArrows.forEach(el => el.classList.remove('offers__info--expand--active'))
+//         allCityList.forEach(el => el.classList.remove('offers__city--list--active'))
+//     }
+//     if(!expandMenu) return
+//     const arrow = expandMenu.closest('div').querySelector('.offers__info--expand--box')
+//     const offerBox = e.target.closest('.offers__box')
+//     const cityList = offerBox.closest('div').querySelector('.offers__city--list')
+//     if(!cityList.classList.contains('offers__city--list--active')){
+//         allCityList.forEach(el => el.classList.remove('offers__city--list--active'))
+//         cityList.classList.add('offers__city--list--active')
+//     }
+//     if(!arrow.classList.contains('offers__info--expand--active')){
+//         allArrows.forEach(el => el.classList.remove('offers__info--expand--active'))
+//         arrow.classList.add('offers__info--expand--active')
 
-    } else {
-        arrow.classList.remove('offers__info--expand--active')
-        cityList.classList.remove('offers__city--list--active')
+//     } else {
+//         arrow.classList.remove('offers__info--expand--active')
+//         cityList.classList.remove('offers__city--list--active')
 
+//     }
+// })
 
-    }
-})
+const offersBox = document.querySelector(".offers__box--container");
+
+offersBox.addEventListener("click", function (e) {
+  const allArrows = document.querySelectorAll(".offers__info--expand--box");
+  const allCityList = document.querySelectorAll(".offers__city--list");
+  const overlay = document.querySelector(".overlay");
+  const clicked = e.target;
+  const curBox = e.target.closest(".offers__box");
+  if (!curBox) {
+    allArrows.forEach((el) =>
+      el.classList.remove("offers__info--expand--active")
+    );
+    allCityList.forEach((el) =>
+      el.classList.remove("offers__city--list--active")
+    );
+    overlay.classList.add("hidden");
+  }
+  const cur = clicked.closest(".offers__info--city");
+
+  if (!cur) {
+    allArrows.forEach((el) =>
+      el.classList.remove("offers__info--expand--active")
+    );
+    allCityList.forEach((el) =>
+      el.classList.remove("offers__city--list--active")
+    );
+    overlay.classList.add("hidden");
+  }
+  if(!cur) return
+  const dropdown = curBox.closest("div").querySelector(".offers__city--list");
+  const arrow = cur.closest("div").querySelector(".offers__info--expand--box");
+  console.log(arrow);
+  if (!arrow) return;
+  if (!arrow.classList.contains("offers__info--expand--active")) {
+    arrow.classList.add("offers__info--expand--active");
+    dropdown.classList.add("offers__city--list--active");
+    overlay.classList.remove("hidden");
+  } else {
+    arrow.classList.remove("offers__info--expand--active");
+    dropdown.classList.remove("offers__city--list--active");
+    overlay.classList.add("hidden");
+  }
+});
 
