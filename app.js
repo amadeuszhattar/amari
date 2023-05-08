5fa843a573782b06da545e19c2f6234f1f29dea8
Mariusz Łazarz <mariusz.lazarz97@gmail.com>
2023-05-08 21:26:19 +0200

    setting up the server

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
-// app.set("view engine", "pug");
+app.set("view engine", "pug");
 app.set("view engine", "html");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.get("/", (req, res) => {
-//   res.render("header.html");
-// });
+app.get("/", (req, res) => {
+  res.render("base");
+});
+
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/header.html`);
 });
