230103006e45698bc90e377d869d09372ba85aca
Amadeusz <amadi888@wp.pl>
2023-05-08 20:24:43 +0100

    cleaning

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const path = require('path')
-const express = require('express')
-const app = express()
+const path = require("path");
+const express = require("express");
+const app = express();
 
 const port = 3001;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
-// using templates
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'views'));  
-app.use(express.static(path.join(__dirname, 'public')));
+// app.set("view engine", "pug");
+app.set("view engine", "html");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-
-app.get('/', (req, res) => {
-    res.render('base')
-  })
+// app.get("/", (req, res) => {
+//   res.render("header.html");
+// });
+app.get("/", (req, res) => {
+  res.sendFile(`${__dirname}/views/header.html`);
+});
