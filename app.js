369960c9bf35cfa1ce19ec9dafdd853afea44118
Amadeusz <amadi888@wp.pl>
2023-05-08 20:29:35 +0100

    cleaning

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
 
 const port = 3001;
@@ -7,15 +7,19 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
 
-app.set("view engine", "pug");
-app.set("view engine", "html");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'pug');
+// app.set('view engine', 'html');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/mariusz", (req, res) => {
-  res.render("base");
+app.get('/mariusz', (req, res) => {
+  res.render('base');
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(`${__dirname}/views/header.html`);
+// app.get('/amadeusz', (req, res) => {
+//   res.sendFile(`${__dirname}/views/header.html`);
+// });
+
+app.get('/amadeusz', (req, res) => {
+  res.render('base2');
 });
